574af4bdedf84945a85f13bd4348a4f856413280
Koichi Taura <taura.koichi@gmail.com>
2019-05-18 21:01:25 +0900

    add tweet entity structure

--- app/domain/tweet.go
+++ app/domain/tweet.go
@@ -1,1 +1,27 @@
 package domain
+
+/*
+Tweet structure to insert mongo db
+*/
+type Tweet struct {
+	ID    int64
+	IDStr string
+	Text  string
+	User
+}
+
+/*
+User structure of twitter
+*/
+type User struct {
+	ID                   int64
+	IDStr                string
+	Name                 string
+	ScreenName           string
+	Location             string
+	URL                  string
+	Description          string
+	FollowersCount       int
+	FriendsCount         int
+	ProfileImageURLHTTPS string
+}
